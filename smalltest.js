add7bea6627e0ff6ceafeb7ad8865bb52a4bf2d5
tempuser1909 <tempuser1909@gmail.com>
2016-01-20 19:58:56 +0800

    Update smalltest.js

--- smalltest.js
+++ smalltest.js
@@ -1,4 +1,8 @@
+console.log('writing something...');
 var fso = new ActiveXObject("Scripting.FileSystemObject");
+console.log('created activexobj');
 var fh  = fso.CreateTextFile("D:\_Repo\smalltest.txt", 8, true);
+console.log('created text file.');
 fh.WriteLine("this is only a smalltest\n");
+console.log('wrote a line');
 fh.Close();
